34c3d37aaad3e647ad654d25ea2e26305a8e585c
Achraftraf <achrafzarouki20@gmail.com>
2024-08-11 20:45:45 +0100

    counter challenge

--- app/client/page.js
+++ app/client/page.js
@@ -1,10 +1,15 @@
-import React from 'react'
-const ClientPage = () => {
-  return (
-    <div>
-        <h1 className='text-7xl'>ClientPage</h1>
-      </div>
-  )
-}
+"use client";
+  import React, { useState } from 'react';
+  const ClientPage = () => {
+    const [count ,setCount] = useState (0)
+    return (
+      <div>
+          <h1 className='text-7xl font-bold mb-4'>{count}</h1>
+          <button className='btn btn-primary' onClick={()=> setCount(count+1)}> 
+            increase
+          </button>
+        </div>
+    )
+  }
 
-export default ClientPage
+  export default ClientPage
--- app/page.js
+++ app/page.js
@@ -4,9 +4,10 @@ import Link from 'next/link'
 const HomePage = () => {
   return (
     <div>
-        <h1 className='text-7xl'>HomePage</h1>
-        <Link href='/about' className='text-2xl'>
-        about page
+      
+        <h1 className='text-5xl mb-8 font-bold'>Next.js Tutorial</h1>
+        <Link href='/client' className='btn btn-accent'>
+        get started
         </Link>
       </div>
   )
