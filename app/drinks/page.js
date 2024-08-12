1b8ef3394d0bd97dc64a92dd1ebf7033005de9d3
achrafzarouki <achrafzarouki20@gmail.com>
2024-08-13 00:13:53 +0100

    i mad a changes in the drinks page and also i add the load page by make it as loading,js and also i daisyui i use the classname loading loading-ring loading-x

--- app/drinks/page.js
+++ app/drinks/page.js
@@ -2,9 +2,15 @@ import React from 'react'
 
 const url = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?f=a';
 
-const DrinksPage = async () => {
+const fetchDrinks = async () => {
+  await new Promise((resolve) => setTimeout(resolve,1000));
   const response = await fetch(url);
   const data = await response.json();
+  return data
+  };
+
+const DrinksPage = async () => {
+const data = await fetchDrinks()
   console.log(data);
   return (
     <div>
