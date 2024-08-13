13e2ab05a3103adc05b13dbb52b60ec979c712c8
achrafzarouki <achrafzarouki20@gmail.com>
2024-08-13 21:58:29 +0100

    to use the remote images we need to set the config of nextjs with the hostname that will be the url of the website

--- app/drinks/[id]/page.js
+++ app/drinks/[id]/page.js
@@ -21,14 +21,14 @@ const SingleDrinkPage = async ({ params }) => {
         back to drinks
       </Link>
 
-      {/* <Image
+      <Image
         src={imgSrc}
         width={300}
         height={300}
         className='w-48 h-48 rounded-lg shadow-lg mb-4'
         priority
         alt={title}
-      /> */}
+      />
       {/* <Image src={drinkImg} className='w-48 h-48 rounded-lg' alt='drink' /> */}
       <h1 className='text-4xl mb-8'>{title}</h1>
     </div>
