2b2de49701916538109da32c3d76e0f5790f6a74
achrafzarouki <achrafzarouki20@gmail.com>
2024-08-17 00:38:56 +0100

    i make the web site responsive by using the 'grid sm:grid-cols-2 gap-6 mt-6' instead of the 'menu menu-vertical pl-0'

--- components/DrinkList.jsx
+++ components/DrinkList.jsx
@@ -1,20 +1,51 @@
-import Link from "next/link";
-import React from "react";
-
-const DrinkList = ({ drinks }) => {
+import Link from 'next/link';
+import Image from 'next/image';
+const DrinksList = ({ drinks }) => {
   return (
-    <ul className="menu menu-vertical pl-0">
-      {drinks.map((drink) => (
-        <li key={drink.idDrink}>
-          <Link
-            href={`/drinks/${drink.idDrink}`}
-            className="text-xl font-medium">
-            {drink.strDrink}
-          </Link>
-        </li>
-      ))}
+    <ul className='grid sm:grid-cols-2 gap-6 mt-6'>
+      {drinks.map((drink) => {
+        return (
+          <li key={drink.idDrink}>
+            <Link
+              href={`/drinks/${drink.idDrink}`}
+              className='text-xl font-medium'
+            >
+              <div className='relative h-48 mb-4'>
+                <Image
+                  src={drink.strDrinkThumb}
+                  fill
+                  sizes='(max-width:768px) 100vw, (max-width:1200px) 50vw'
+                  alt={drink.strDrink}
+                  className='rounded-md object-cover'
+                />
+              </div>
+              {drink.strDrink}
+            </Link>
+          </li>
+        );
+      })}
     </ul>
   );
 };
+export default DrinksList;
+// import Link from 'next/link';
 
-export default DrinkList;
+// const DrinksList = ({ drinks }) => {
+//   return (
+//     <ul className='menu menu-vertical pl-0'>
+//       {drinks.map((drink) => {
+//         return (
+//           <li key={drink.idDrink}>
+//             <Link
+//               href={`/drinks/${drink.idDrink}`}
+//               className='text-xl font-medium'
+//             >
+//               {drink.strDrink}
+//             </Link>
+//           </li>
+//         );
+//       })}
+//     </ul>
+//   );
+// };
+// export default DrinksList;
