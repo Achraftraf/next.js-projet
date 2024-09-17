4312392c9c9276316b301168ac41e56cee8399b7
achrafzarouki <achrafzarouki20@gmail.com>
2024-09-17 18:29:15 +0100

    safe push

--- components/DeleteForm.jsx
+++ components/DeleteForm.jsx
@@ -1,10 +1,21 @@
-import { deleteTask } from '../app/utils/actions';
+'use client';
+import { useFormStatus } from 'react-dom';
+import { deleteTask } from 'app/utils/actions.js';
+
+const SubmitBtn = () => {
+  const { pending } = useFormStatus();
+  return (
+    <button type='submit' className='btn btn-xs btn-error' disabled={pending}>
+      {pending ? 'pending...' : 'delete'}
+    </button>
+  );
+};
 
 const DeleteForm = ({ id }) => {
   return (
     <form action={deleteTask}>
       <input type='hidden' name='id' value={id} />
-      <button className='btn btn-error btn-xs'>delete</button>
+      <SubmitBtn />
     </form>
   );
 };
