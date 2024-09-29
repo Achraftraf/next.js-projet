ebc0095992271f226ab0f37a73af98beeab6c600
Achraftraf <achrafzarouki20@gmail.com>
2024-09-29 15:45:40 +0100

    i create the route.js to handle the api get to get all the tasks

---
+++ app/api/tasks/route.js
@@ -0,0 +1,9 @@
+
+import { NextResponse } from 'next/server';
+import db from 'app/utils/db.ts';
+
+export const GET = async (request) => {
+  const tasks = await db.task.findMany();
+  return Response.json({ tasksData: tasks });
+  // return NextResponse.json({ data: tasks });
+};
